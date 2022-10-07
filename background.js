e57f4541c0ee5a7d9f6e57eaca81da93e3aa8a34
SHIV SANKAR <72968797+shiv1305@users.noreply.github.com>
2022-10-07 13:11:29 +0530

    AdBlocker Commit

---
+++ background.js
@@ -0,0 +1,24 @@
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) { return { cancel: true } }, { urls: blockUrl }, ["blocking"]
+)
+
+
+blockUrl = [
+    "*://*.googlesyndication.com/*",
+    "*://*.adservice.google.co.in/*",
+    "*://*.adservice.google.com/*",
+    "*://*.adservice.google.com.ua/*",
+    "*://*.googleusercontent.com/*",
+    "*://*.feedburner.google.com/*",
+    "*://*.googleads2.g.doubleclick.net/*",
+    "*://*.googleads4.g.doubleclick.net/*",
+    "*://*.googleadservices.com/*",
+    "*://*.googleads.g.doubleclick.net/*",
+    "*://*.google-analytics.com/*",
+    "*://*.googleplaysetvices.com/*",
+    "*://*.googletagmanager.com/*",
+    "*://*.googletagservices.com/*",
+    "*://*.google.unbounce.com/*"
+
+
+]
